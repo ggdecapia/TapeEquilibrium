513f3760a96cd3aa68ebe686bb51870e9a91d75b
Glenda Decapia <ggdecapia@yahoo.com>
2021-03-02 09:50:02 -0600

    added test data scenario details

--- TestScenarios.py
+++ TestScenarios.py
@@ -1,11 +1,17 @@
 # Test Inputs
+# test_one_of_the_element_is_a_negative_value
 [-3, 1, 2, 4, 3]
+# test_when_all_the_elements_are_negative_values
 [-3, -1, -2, -4, -3]
+# test_boundary_values_negative_first
 [-1000,1000]
+# test_negative_boundary_values_only
 [-1000,-1000]
+# test_boundary_values_positive_first
 [1000,-1000]
+# test_positive_boundary_values_only
 [1000,1000]
-[-1000,0]
+# test_zero_and_negative_boundary
 [0,-1000]
+# test_zero_and_positive_boundary
 [1000,0]
-[0,1000]
